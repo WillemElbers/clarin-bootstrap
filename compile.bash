98bf20196111d3b32c1311f08d9c3a97f4a2a1cd
Willem Elbers <wjm.elbers@gmail.com>
2023-05-26 12:37:05 +0200

    Removed dist folder again

--- compile.bash
+++ compile.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mkdir -p dist/node_modules/bootstrap
-sass --style=compressed scss/clarin.scss dist/clarin-bootstrap.css
-cp -r node_modules/bootstrap/scss dist/node_modules/bootstrap/
+sass --style=compressed scss/clarin.scss clarin-bootstrap.css
+
